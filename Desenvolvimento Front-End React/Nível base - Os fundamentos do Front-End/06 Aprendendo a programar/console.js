3cd0305c5c77cc1f35754cf15f8a8f0350e14756
Gustavo <gualencarj@gmail.com>
2025-12-03 19:14:16 -0300

    Criando variáveis e atualizando valores

--- Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/06 Aprendendo a programar/console.js
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/06 Aprendendo a programar/console.js
@@ -1,1 +1,11 @@
-console.log('Olá, mundo do Node.JS!');
+idade = 36
+
+dataAniversario = '8 de julho'
+
+console.log('Minha idade atual: ', idade)
+
+console.log('Data de aniversário: ', dataAniversario)
+
+idade = 37
+
+console.log('Minha idade depois do aniversário: ', idade)
