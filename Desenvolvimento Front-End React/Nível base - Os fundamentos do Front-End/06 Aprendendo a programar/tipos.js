eeda6ed7b65cd5338d9e60ad79769c10fb082ab1
Gustavo <gualencarj@gmail.com>
2025-12-03 19:25:57 -0300

    Identificando os tipos de variáveis

---
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/06 Aprendendo a programar/tipos.js
@@ -0,0 +1,23 @@
+let nome = 'Ana';
+let idade = 23;
+let altura = 1.67;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof altura);
+
+let estaChovendo = true;
+
+console.log(typeof estaChovendo);
+
+let cidade;
+
+console.log(typeof cidade);
+
+let aprendeuJS = null;
+
+console.log(typeof aprendeuJS);
+
+let simbolo = Symbol();
+
+console.log(typeof simbulo);
