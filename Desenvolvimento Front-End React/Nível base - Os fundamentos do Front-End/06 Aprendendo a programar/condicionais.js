459b705872655f4b4c10a17786c2ebe67ac3c84b
Gustavo <gualencarj@gmail.com>
2025-12-09 16:50:37 -0300

    Entendendo if, else if e else

---
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/06 Aprendendo a programar/condicionais.js
@@ -0,0 +1,30 @@
+
+const idade = 17;
+
+if (idade >= 18) {
+	console.log("É maior de idade");
+} else {
+	console.log("É menor de idade");
+}
+
+const notaAluno = 8;
+
+if (notaAluno >= 9) {
+	console.log("Nota excelente");
+} else if (notaAluno >= 7) {
+	console.log("Nota boa");
+} else if (notaAluno >= 5) {
+	console.log("Nota mediana");
+} else {
+	console.log("Nota ruim");
+}
+
+const temperatura = 36;
+
+if (temperatura >= 37) {
+	console.log("Febre");
+} else if (temperatura >= 35) {
+	console.log("Temperatura normal");
+} else {
+	console.log("Temperatura baixa");
+}
