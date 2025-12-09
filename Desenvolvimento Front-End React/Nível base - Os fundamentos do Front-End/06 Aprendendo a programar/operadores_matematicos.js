5dc410450984d80e662b1c2554bdc17c911bc413
Gustavo <gualencarj@gmail.com>
2025-12-09 16:30:26 -0300

    Operadores aritiméticos

---
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/06 Aprendendo a programar/operadores_matematicos.js
@@ -0,0 +1,38 @@
+const numeroUm = 4;
+const numeroDois = 8;
+
+console.log("Subtração:", numeroDois - numeroUm);
+
+const subtracao = numeroDois - numeroUm;
+console.log("Subtração (variável):", subtracao);
+
+const soma = numeroUm + numeroDois;
+console.log("Soma:", soma);
+
+const divisao = numeroDois / numeroUm;
+console.log("Divisão:", divisao);
+
+const multiplicacao = numeroDois * numeroUm;
+console.log("Multiplicação:", multiplicacao);
+
+const contaComplexa = (numeroUm + numeroDois) * numeroDois;
+console.log("Conta complexa:", contaComplexa);
+
+const resto = 11 % 2;
+console.log("Resto da divisão (11 % 2):", resto);
+
+let contador = 0;
+contador++;
+console.log("Incremento:", contador);
+contador--;
+console.log("Decremento:", contador);
+
+let numeroTres = 2;
+numeroTres += 3;
+console.log("Atribuição +=  (2 + 3):", numeroTres);
+numeroTres *= 2;
+console.log("Atribuição *=:", numeroTres);
+
+const resultadoFinal = numeroTres + (numeroDois / numeroUm) - (11 % 2);
+console.log("Resultado final:", resultadoFinal);
+
