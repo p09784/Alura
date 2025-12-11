ae63fd34f784b6944ae505e9ba4c53766f6d5aa0
Gustavo <gualencarj@gmail.com>
2025-12-11 19:54:34 -0300

    Métodos de objetos e for in

---
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/06 Aprendendo a programar/metodosObjetos.js
@@ -0,0 +1,23 @@
+const pessoa = {
+  nome: 'Vinícius',
+  idade: 37,
+  pets: ['Luke'],
+  nacionalidade: 'Brasileiro',
+};
+
+for (const chave in pessoa) {
+  console.log('Chave:', chave);
+}
+
+for (const chave in pessoa) {
+  console.log('Chave:', chave);
+  console.log('Valor:', pessoa[chave]);
+}
+
+const chaves = Object.keys(pessoa);
+const valores = Object.values(pessoa);
+const entradas = Object.entries(pessoa);
+
+console.log('\nChaves:', chaves);
+console.log('Valores:', valores);
+console.log('Entradas (chave/valor):', entradas);
