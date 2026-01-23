34a21f7b55a625c71fb703a3ef8eb27855c76438
Gustavo <gualencarj@gmail.com>
2026-01-23 17:19:18 -0300

    Prática: if e else

---
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/07 Condicionais/exercicios.js
@@ -0,0 +1,33 @@
+// IF/ELSE
+
+// definir um fluxo para somar ou multiplicar dois números usando if/else
+
+const num1 = 2;
+const num2 = 4;
+const operacao = 'divisão';
+
+if (operacao === 'soma') {
+    console.log(num1 + num2);
+} else if (operacao === 'multiplicação') {
+    console.log(num1 * num2);
+} else {
+    console.log('Operação não identificada');
+}
+
+// localizar o nivel de bônus de acordo com a faixa salarial 
+// ex: $11000 e acima: 3% de bônus 
+// $10999 a $7000: 5% de bônus 
+// $ 6999 a $4008: 7% de bônus 
+// $3999 p baixo: 9% de bônus 
+
+const salarіо = 2000;
+
+if (salario >= 11000) {
+    console.log('3% de bônus');
+} else if (salario < 11000 && salario >= 7000) {
+    console.log('5% de bônus');
+} else if (salario < 7000 && salario >= 4000) {
+    console.log('7% de bônus');
+} else {
+    console.log('9% de bônus');
+}
