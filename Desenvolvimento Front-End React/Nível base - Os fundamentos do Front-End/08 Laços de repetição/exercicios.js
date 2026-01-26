b101927cea3c288ce2daac57ae148cff64d02521
Gustavo <gualencarj@gmail.com>
2026-01-26 13:47:50 -0300

    Prática For

---
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/08 Laços de repetição/exercicios.js
@@ -0,0 +1,18 @@
+for (let contador = 1; contador <= 30; contador++) {
+    const numero = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+    if (numero === 15) {
+        console.log(`${numero} em ${contador} tentativas`);
+        break;
+    }
+}
+
+
+for (let i = 1, contador = 0; i <= 30; i++) {
+    const numero = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+    if (numero % 5 === 0) {
+        continue;
+    }
+    contador++;
+}
+
+console.log(contador);
