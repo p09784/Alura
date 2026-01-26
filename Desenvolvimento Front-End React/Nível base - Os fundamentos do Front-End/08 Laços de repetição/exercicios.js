a648547509250b7c671251b92ebf495bb2dea802
Gustavo <gualencarj@gmail.com>
2026-01-26 13:53:27 -0300

    Prática: While

--- Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/08 Laços de repetição/exercicios.js
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/08 Laços de repetição/exercicios.js
@@ -7,6 +7,7 @@ for (let contador = 1; contador <= 30; contador++) {
 }
 
 
+
 for (let i = 1, contador = 0; i <= 30; i++) {
     const numero = Math.floor(Math.random() * (50 - 1 + 1) + 1);
     if (numero % 5 === 0) {
@@ -16,3 +17,48 @@ for (let i = 1, contador = 0; i <= 30; i++) {
 }
 
 console.log(contador);
+
+
+
+
+
+
+const numeroSecreto = 8;
+let numeroAleatorio = 0;
+let tentativas = 0;
+
+while (numeroSecreto !== numeroAleatorio) {
+    numeroAleatorio = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+    tentativas++;
+}
+
+console.log(`adivinhou em ${tentativas} tentativas`);
+
+
+
+let numeroRandom = 0;
+
+do {
+    numeroRandom = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+    console.log(numeroRandom);
+} while (numeroRandom % 2 !== 0);
+
+console.log(numeroRandom);
+
+
+
+const texto = 'luz azul';
+
+let textoInvertido = '';
+
+for (let i = texto.length - 1; i >= 0; i--) {
+    textoInvertido += texto[i];
+}
+
+console.log(textoInvertido);
+
+const result = texto === textoInvertido
+    ? `${texto} é palíndromo`
+    : `${texto} não é palíndromo`;
+        
+console.log(result);
