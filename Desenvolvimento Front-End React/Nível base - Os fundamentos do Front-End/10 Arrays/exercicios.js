ddb4ea6a77d82009e42eeba8b9771c6ded6825da
Gustavo <gualencarj@gmail.com>
2026-01-28 17:07:11 -0300

    Iteração e criação de arrays utilizando o laço de repetição for

--- Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/10 Arrays/exercicios.js
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/10 Arrays/exercicios.js
@@ -8,3 +8,18 @@ console.log(arr[3]);
 arr[5] = 10;
 delete arr[5];
 console.log(arr);
+
+
+
+function criaArrNum(inicio, fim) {
+  const arr = [];
+  let elemento = inicio;
+
+  for(let i = 0, j = 0; j < fim; i++, elemento++) {
+    arr[i] = elemento;
+    j = elemento;
+  }
+  return arr;
+}
+
+console.log(criaArrNum(456, 560));
