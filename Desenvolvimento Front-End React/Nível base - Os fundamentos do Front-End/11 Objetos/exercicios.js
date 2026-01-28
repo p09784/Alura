17d28830e2e67460479985bfec0c6a947a1de512
Gustavo <gualencarj@gmail.com>
2026-01-28 17:55:02 -0300

    Métodos object

--- Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/11 Objetos/exercicios.js
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/11 Objetos/exercicios.js
@@ -25,3 +25,25 @@ function exibeTrofeus(listaTrofeus) {
 }
 
 exibeTrofeus(pessoa.trofeus);
+
+
+
+const clientes = [
+    {
+        nome: 'Marina M',
+        ativo: true,
+    },
+    {
+        nome: 'Rafael R',
+        ativo: false,
+    },
+    {
+        nome: 'Fabio F',
+        ativo: false,
+    },
+];
+
+for (const cliente of clientes) {
+    const status = cliente.ativo ? 'ativo' : 'inativo';
+    console.log(`status cliente ${cliente.nome}: ${status}`);
+}
