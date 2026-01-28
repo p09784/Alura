a8de263fb9759e87804a3e1cd837081f822ed981
Gustavo <gualencarj@gmail.com>
2026-01-28 17:23:16 -0300

    Métodos de array

--- Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/10 Arrays/exercicios.js
+++ Desenvolvimento Front-End React/Nível base - Os fundamentos do Front-End/10 Arrays/exercicios.js
@@ -23,3 +23,33 @@ function criaArrNum(inicio, fim) {
 }
 
 console.log(criaArrNum(456, 560));
+
+
+
+const arr1 = [[1, 2], 2, 3];
+
+const copiaArray = (arr) => {
+  const copia = [];
+  arr.forEach((elem) => {
+    if (Array.isArray(elem)) {
+      copia.push(copiaArray(elem));
+    } else {
+      copia.push(elem);
+    }
+  })
+  return copia;
+}
+
+const arr2 = copiaArray(arr1);
+arr1[0][0] = 5;
+
+console.log(arr1);
+console.log(arr2);
+
+
+
+const cpfs = ['12323434545', '34658756786', 43254365434, '76547865345', 56768767564];
+
+const result = cpfs.map(cpf => typeof cpf === 'string' ? cpf : cpf.toString());
+
+console.log(result);
